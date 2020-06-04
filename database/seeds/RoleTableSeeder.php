bc8fe82c71d9d7be49f316de5562c38fe705c998
m971119 <m971119@gmail.com>
2020-06-04 11:57:57 +0800

    added seeder

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $roles = [
+            ['name' => 'administrator'],
+            ['name' => 'subscriber'],
+            ['name' => 'author']
+        ];
+
+        foreach ($roles as $role) {
+            DB::table('roles')->insertOrIgnore($role);
+        }
+    }
+}
